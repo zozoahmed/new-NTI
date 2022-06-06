a4ff7599fac135b6406c2615ca60abb6527ead79
zeinab-Ahmed <44482247+zozoahmed@users.noreply.github.com>
2022-06-06 18:24:48 +0200

    Add files via upload

---
+++ day2.php
@@ -0,0 +1,48 @@
+<?php
+  
+//task day2
+ // 1
+// $char = 'a';
+// $next_char = ++$char; 
+
+// if (strlen($next_char) > 1) 
+// {
+//  $next_char = $next_char[0];
+//  }
+// echo $next_char."\n";
+
+
+
+$char = 'z';
+$next_char = ++$char; 
+
+if (strlen($next_char) > 1) 
+{
+ $next_char = $next_char[0];
+ }
+echo $next_char."\n";
+
+
+
+ //task2
+
+// $my_url = 'http://www.example.com/5478631';
+// echo substr($my_url, strrpos($my_url, '/' )+1)."\n";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+<php?>
+
+    <body> 
+    </body>
+</html>
